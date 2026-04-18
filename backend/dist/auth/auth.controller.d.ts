f1f0aaa56781763a98f03278ab7e23791b2b70e8
khamushu <khamushu@khamushus-MacBook-Air.local>
2026-04-18 11:00:01 +0700

    remove dist folder

--- backend/dist/auth/auth.controller.d.ts
+++
@@ -1,9 +0,0 @@
-import type { Response } from 'express';
-import { AuthService } from './auth.service';
-export declare class AuthController {
-    private readonly authService;
-    constructor(authService: AuthService);
-    googleLogin(): void;
-    googleCallback(req: any, res: Response): void;
-    me(req: any): any;
-}
--- backend/dist/auth/auth.service.d.ts
+++
@@ -1,9 +0,0 @@
-import { JwtService } from '@nestjs/jwt';
-import { User } from '../users/user.entity';
-export declare class AuthService {
-    private readonly jwtService;
-    constructor(jwtService: JwtService);
-    login(user: User): {
-        access_token: string;
-    };
-}
